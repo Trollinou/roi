852dd5931411c60f28f21f02abc62ad60e59fcef
Etienne Gagnon <etienne.gagnon@gmail.com>
2025-09-17 15:03:00 +0200

    Update roi.php

--- roi.php
+++ roi.php
@@ -4,13 +4,12 @@
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Ressources et Organisation pour l’Initiation aux échecs.
  * Version:           1.0.0
- * Requires at least: 5.2
- * Requires PHP:      7.2
+ * Requires at least: 6.8
+ * Requires PHP:      8.2
  * Author:            Etienne Gagnon
- * Author URI:        https://author.example.com/
+ * Author URI:        
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
  * Text Domain:       roi
  * Domain Path:       /languages
  * Depends:           dame
