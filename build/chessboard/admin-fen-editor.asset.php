344eec24b52f8381a82c86b51fc1df30609ae68e
Etienne Gagnon <etienne.gagnon@gmail.com>
2026-07-17 11:18:22 +0200

    fix(diagramme): global monkey-patch of BoardCore setPosition for invalid/partial FEN support in back-office

--- build/chessboard/admin-fen-editor.asset.php
+++ build/chessboard/admin-fen-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime'), 'version' => '11645460bb748624deb4');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime'), 'version' => '952e98caffdf5e488e23');
